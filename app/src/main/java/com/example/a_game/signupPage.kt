586beced7a98ec43741b0ff2ad1e7cf742635a2d
ceciliaisawesome <gichiacess04@gmail.com>
2023-06-21 16:57:31 +0300

    added text file

---
+++ app/src/main/java/com/example/a_game/signupPage.kt
@@ -0,0 +1,11 @@
+package com.example.a_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signupPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
