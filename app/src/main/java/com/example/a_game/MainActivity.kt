aede6346351fd400adefc4167eaaa09dbc194623
ceciliaisawesome <gichiacess04@gmail.com>
2023-06-23 16:19:01 +0300

    added test file

--- app/src/main/java/com/example/a_game/MainActivity.kt
+++ app/src/main/java/com/example/a_game/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.a_game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Btnsignup:Button
+    lateinit var Txtlogin:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Btnsignup=findViewById(R.id.Btn_signup)
+        Txtlogin=findViewById(R.id.Txt_login)
+
+        Btnsignup.setOnClickListener {
+            val intent=Intent(this, signupPage::class.java)
+            startActivity(intent)
+        }
+        Txtlogin.setOnClickListener {
+            val intent=Intent(this, Loginpage::class.java)
+            startActivity(intent)
+        }
     }
 }
